4681102d21b40c0c3745aa0679c504984bb09f2a
jsmith <jsmith@dirextion.com>
2014-04-24 15:54:47 -0500

    - committing changes to page controller and node classes in working state, create works, edit works.

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -6,6 +6,7 @@
  * @author
  * @version 
  */
+
 require_once 'Zend/Controller/Action.php';
 
 class PageController extends Zend_Controller_Action
@@ -33,6 +34,7 @@ class PageController extends Zend_Controller_Action
                 $itemPage->headline = $data['headline'];
                 $itemPage->description = $data['description'];
                 $itemPage->content = $data['content'];
+                $itemPage->image = $data['image'];
                 // upload the image
 //                 if($pageForm->image->isUploaded()){
 //                     $pageForm->image->receive();
@@ -72,10 +74,10 @@ class PageController extends Zend_Controller_Action
         $pageForm = new Cms_Form_ManagePage();
         $pageForm->setAction('/page/edit');
         if($this->_request->isPost()) {
-            if($pageForm->isValid($this->_request->getPost)) {
+            if($pageForm->isValid($this->_request->getPost())) {
                 
                 $data = $pageForm->getValues();
-                
+                //Zend_Debug::dump($data);
                 $itemPage->name = $data['name'];
                 $itemPage->headline = $data['headline'];
                 $itemPage->description = $data['description'];
@@ -91,5 +93,18 @@ class PageController extends Zend_Controller_Action
                 return $this->_forward('list');
             }
         }
+        else {
+        	// create the image preview 
+        	$imagePreview = $pageForm->createElement('image', 'image_preview');
+        	// element options 
+        	$imagePreview->setLabel('Preview Image: '); 
+        	$imagePreview->setAttrib('style', 'width:200px;height:auto;'); 
+        	// add the element to the form 
+        	$imagePreview->setOrder(4); 
+        	$imagePreview->setImage('/modules/cms/content/'.$itemPage->image); 
+        	$pageForm->addElement($imagePreview);
+        	$pageForm->populate($itemPage->toArray());
+        }
+        $this->view->form = $pageForm;
     }
 }
